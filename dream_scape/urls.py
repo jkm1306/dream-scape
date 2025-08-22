be9d87ab40587cb94d3423572709a9984eee9a29
Nashiol Nyirongo <nyirongonashiol@gmail.com>
2025-08-23 00:43:58 +0200

    Set up the file structure properly.

--- dream_scape/urls.py
+++ dream_scape/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include ('services.urls')), #For home and other services related urls
+    path('', include ('services.urls')),
     path('users/', include ('users.urls')), 
     path('student_travel/', include ('student_travel.urls')), 
 ]
--- student_travel/urls.py
+++ student_travel/urls.py
@@ -6,7 +6,7 @@ app_name = 'student_travel'
 urlpatterns = [
     path('students/', views.students, name='students'), 
     path('tourists/', views.tourists, name='tourists'), 
-    path("students/student-application/", views.student_application_view, name="student_application"),
-    path("tourists/tourist-inquiry/", views.tourist_inquiry_view, name="tourist_inquiry"),
+    path('students/student-application/', views.student_application_view, name='student_application'),
+    path('tourists/tourist-inquiry/', views.tourist_inquiry_view, name='tourist_inquiry'),
 ]
 
